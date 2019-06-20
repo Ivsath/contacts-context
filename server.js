4a5495771ae89098fc2659c1b90e6d44d9148ca2
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-19 22:10:23 -0500

    Create backend routes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const contacts = require("./routes/contacts");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the jungle! " });
-});
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/contacts", contacts);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
