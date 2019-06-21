837449d51169bfa72ceebb65eb06fd394a37b698
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-20 21:48:17 -0500

    Add missing route

--- routes/contacts.js
+++ routes/contacts.js
@@ -16,10 +16,17 @@ router.post("/", (req, res) => {
   res.send("Add contact");
 });
 
+// @route   PUT /api/contacts/:id
+// @desc    Update contact
+// @access  Private
+router.put("/:id", (req, res) => {
+  res.send("Update contact");
+});
+
 // @route   DELETE /api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.put("/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.send("Delete contact");
 });
 
