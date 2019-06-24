48ba878f698b0ddba80787e12e1a493cbec5d542
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-24 13:35:20 -0500

    Create the ContactForm component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -42,7 +42,10 @@ const ContactState = props => {
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
-  // Add contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete contact
 
@@ -57,7 +60,7 @@ const ContactState = props => {
   // Clear filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
       {props.children}
     </ContactContext.Provider>
   );
