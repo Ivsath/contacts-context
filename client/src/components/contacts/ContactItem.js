8156ff7a9682040356d8c55eea282ecf33aa8c22
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-24 03:17:35 -0500

    Create Contacts and ContactItem components

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ContactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
+
+  const capitalize = string => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{" "}
+        <span
+          style={{ float: "right" }}
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {capitalize(type)}
+        </span>
+      </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open" /> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone" /> {phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button className="btn btn-dark btn-sm">Edit</button>
+        <button className="btn btn-danger btn-sm">Delete</button>
+      </p>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default ContactItem;
