c2f88fd534f9992266653c8e8942a3ab078b5978
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-21 11:43:14 -0500

    Fix typo

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -6,7 +6,7 @@ const Alerts = () => {
   const alertContext = useContext(AlertContext);
 
   return (
-    alertContext.alert.length > 0 &&
+    alertContext.alerts.length > 0 &&
     alertContext.alerts.map((alert) => (
       <div key={alert.id} className={`alert alert-${alert.type}`}>
         <i className="fas fa-info-circle"></i> {alert.msg}
