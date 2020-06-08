102b9362e1ed5ac7d5ed3688e2a2aa75914a7b3a
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-08 14:33:56 -0500

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const users = require("./routes/users");
 const auth = require("./routes/auth");
@@ -16,5 +17,15 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/contacts", contacts);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
